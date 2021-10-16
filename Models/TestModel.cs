6343a246bfd9b27240e7348a85cbcb64dcf6627b
Himanshu Garg <himanshu.garg@nagarro.com>
2021-10-16 18:15:09 +0530

    Add project files.

---
+++ Models/TestModel.cs
@@ -0,0 +1,27 @@
+﻿using CsvHelper.Configuration;
+using System;
+
+namespace SyncFusion.Models
+{
+    public class Model
+    {
+        public DateTime Date { get; set; }
+        public float High { get; set; }
+        public float Low { get; set; }
+        public float Open { get; set; }
+        public float Close { get; set; }
+    }
+
+    public class ModelClassMap : ClassMap<Model>
+    {
+        public ModelClassMap()
+        {
+            _ = Map(m => m.Date).Name(" DATE1");
+            _ = Map(m => m.High).Name(" HIGH_PRICE");
+            _ = Map(m => m.Low).Name(" LOW_PRICE");
+            _ = Map(m => m.Open).Name(" OPEN_PRICE");
+            _ = Map(m => m.Close).Name(" CLOSE_PRICE");
+        }
+    }
+
+}
---
+++ ViewModels/TestViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CsvHelper;
+using SyncFusion.Models;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace SyncFusion.ViewModels
+{
+    public class TestViewModel
+    {
+        public TestViewModel()
+        {
+            using CsvReader csvReader = new(new StreamReader(@"C:\Users\himanshugarg\Desktop\Data1.csv"), CultureInfo.InvariantCulture);
+            _ = csvReader.Context.RegisterClassMap<ModelClassMap>();
+            StockPriceDetails = new ObservableCollection<Model>(csvReader.GetRecords<Model>().ToList());
+        }
+
+        public ObservableCollection<Model> StockPriceDetails { get; set; }
+
+    }
+}
